b5631b9750d23e49e8b41682a800b40966284ced
Roman Krava <romankrava123@gmail.com>
2019-05-01 19:22:38 +0300

    Home_work2_task1

---
+++ Home_work_2_task1.py
@@ -0,0 +1,36 @@
+Python='''
+The Zen of Python, by Tim Peters
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!'''
+
+res_better = Python.find("better")
+print("better:", res_better )
+
+res_never = Python.find("never")
+print("never:", res_never)
+
+res_is = Python.find("is")
+print("is:", res_is)
+
+upper_str = Python.upper()
+print(upper_str)
+
+replace_str = Python.replace('i','&')
+print (replace_str)

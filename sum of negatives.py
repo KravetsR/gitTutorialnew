be9cad6c60e63d3dd00085c0361d25b49c865552
Roman Krava <romankrava123@gmail.com>
2019-05-20 14:15:53 +0300

    pos and neg

--- sum of negatives.py
+++ sum of negatives.py
@@ -1,14 +1,14 @@
 def count_positives_sum_negatives(arr):
-    positive_num =[]
-    negative_num = []
-    result = []
-    for x in arr:
-        if x>0:
-            positive_num.append(x)
-            p = len(positive_num)
-        elif x<0:
-            negative_num.append(x)
-            n = sum(negative_num)
-    result.append(p)
-    result.append(n)
-    return result
+    if not arr: 
+        return []
+    x = 0
+    b
+    y = 0
+    for i in arr:
+        if i > 0:
+            x += 1
+        elif i < 0:
+            y += i
+    return [x, y]
+
+   

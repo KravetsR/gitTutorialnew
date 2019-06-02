ee0f6b0f0637e6b34a6d2b5adb9b747b70792690
Roman Krava <romankrava123@gmail.com>
2019-06-02 23:08:32 +0300

    ball

---
+++ Regular Ball Super Ball.py
@@ -0,0 +1,34 @@
+class Ball(object):
+    def __init__(self, super ='regular'):
+        self.ball_type = super
+
+
+
+
+
+
+
+
+# class Person:
+#         name = 'Ivan'
+#         age = '10'
+
+#     def set(self, name, age ):
+#                 self.name = name 
+#                 self.age = age
+# class Student( Person):
+#         course = 1
+
+
+# igor = Student()
+# igor.set('Ігор', 30)
+# print(igor.name)
+
+
+# vlad = Person ()
+# vlad.set('Влад',25)
+# print(vlad.name + ' ' + str(vlad.age))
+
+# ivan = Person ()
+# ivan.set('Іван', 56)
+# print(ivan.age)

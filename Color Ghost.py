62bc0688cd133ed961f30452815bb9cb2ee3a45e
Roman Krava <romankrava123@gmail.com>
2019-06-03 00:15:51 +0300

    Color

---
+++ Color Ghost.py
@@ -0,0 +1,35 @@
+import random
+class Ghost(object):
+    def __init__(self):
+       self.color = random.choice(["white", "yellow", "purple", "red"])
+
+
+
+
+
+
+
+
+# class Person:
+#         name = 'Ivan'
+#         age = '10'
+
+#     def set(self, name, age ):
+#                 self.name = name 
+#                 self.age = age
+# class Student( Person):
+#         course = 1
+
+
+# igor = Student()
+# igor.set('Ігор', 30)
+# print(igor.name)
+
+
+# vlad = Person ()
+# vlad.set('Влад',25)
+# print(vlad.name + ' ' + str(vlad.age))
+
+# ivan = Person ()
+# ivan.set('Іван', 56)
+# print(ivan.age)

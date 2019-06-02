2d599c07b2656b09030f85beca985eec15daf1e4
Roman Krava <romankrava123@gmail.com>
2019-06-02 23:51:03 +0300

    Adam and Eve

---
+++ Basic subclasses - Adam and Eve.py
@@ -0,0 +1,43 @@
+class Human():
+    pass
+        
+
+class Man(Human):
+    pass
+
+class Woman(Human):
+    pass
+
+def God():
+    Adam = Man()
+    Eva = Woman()
+    names = [Adam, Eva]
+    return names
+
+
+
+
+
+# class Person:
+#         name = 'Ivan'
+#         age = '10'
+
+#     def set(self, name, age ):
+#                 self.name = name 
+#                 self.age = age
+# class Student( Person):
+#         course = 1
+
+
+# igor = Student()
+# igor.set('Ігор', 30)
+# print(igor.name)
+
+
+# vlad = Person ()
+# vlad.set('Влад',25)
+# print(vlad.name + ' ' + str(vlad.age))
+
+# ivan = Person ()
+# ivan.set('Іван', 56)
+# print(ivan.age)
